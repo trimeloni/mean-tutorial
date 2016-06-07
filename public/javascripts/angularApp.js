c39df6d0cdef56b3128fa498a50decab514378c1
Adam Trimeloni <adam@trimeloni.com>
2016-06-06 21:18:29 -0400

    rockin something

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,17 +1,34 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }
+]);
+
+app.factory('posts',[function(){
+  var o = {
+    posts: []
+  };
+  return o;
+
+}]);
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
+'$scope','posts',
+function($scope,posts){
 
-  $scope.posts = [
-   { title: 'post 1', upvotes: 5},
-   { title: 'post 2', upvotes: 2},
-   { title: 'post 3', upvotes: 15},
-   { title: 'post 4', upvotes: 9},
-   { title: 'post 5', upvotes: 4},
-
-  ];
+  $scope.posts = posts.posts;
 
   $scope.addPost = function(){
     // reject the entry if it is empty
